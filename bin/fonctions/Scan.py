75f2850cceeec62e7ac0f32624fb75f48b0390d1
Shawn <smila226@gmail.com>
2020-10-24 21:28:32 +0200

    Version 1 of discovery

--- bin/fonctions/Scan.py
+++ bin/fonctions/Scan.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 import ipaddress
+from colorama import Fore, Style
 from pythonping import ping
+from datetime import datetime
 
 verbosity = {"level": 0}
 
@@ -10,6 +12,30 @@ def discover_network(network):
 
     hosts = list(ipaddress.ip_network(final_nework).hosts())
 
+    nbr_host_tested = 0
+    nbr_host_alive = 0
     for host in hosts:
-        response = ping(str(host), size=10, count=5)
-        print(response)
+        response = ping(str(host), size=1, count=1)
+
+        nbr_host_tested += 1
+
+        if verbosity["level"] == 1:
+            if response.success():
+                nbr_host_alive += 1
+                print(Fore.GREEN + "Success ! 1 host discovered - ip address :"\
+                 " {}".format(host))
+        elif verbosity["level"] == 2:
+            nbr_host_alive += 1
+            if response.success():
+                print(Fore.GREEN + "1 host discovered - ip address : {}".format(
+                host))
+            else:
+                print(Fore.RED + "Warning ! No host at {}".format(host))
+            print(Style.RESET_ALL, end="")
+            print("{} host(s) tested at {}".format(
+            nbr_host_tested, datetime.now().strftime("%H:%M:%S")))
+            print("")
+        else:
+            if response.success():
+                nbr_host_alive += 1
+                print(host)
